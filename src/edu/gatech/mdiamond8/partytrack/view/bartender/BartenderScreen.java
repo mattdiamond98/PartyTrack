cabef12a4456aec6f7985e77750f719f34729537
Matan Diamond <mattdiamond98@gmail.com>
2018-04-08 05:17:34 -0400

    I don't think this is worth putting on my resume anymore

--- src/edu/gatech/mdiamond8/partytrack/view/bartender/BartenderScreen.java
+++ src/edu/gatech/mdiamond8/partytrack/view/bartender/BartenderScreen.java
@@ -4,17 +4,16 @@ import edu.gatech.mdiamond8.partytrack.Config;
 import edu.gatech.mdiamond8.partytrack.model.Drink;
 import edu.gatech.mdiamond8.partytrack.model.user.Attendee;
 import edu.gatech.mdiamond8.partytrack.view.HomeScreen;
+import edu.gatech.mdiamond8.partytrack.view.bouncer.DrinkData;
 import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.value.ChangeListener;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.geometry.Insets;
 import javafx.scene.Group;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
+import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Font;
@@ -62,6 +61,22 @@ public class BartenderScreen {
 
         table.setItems(data);
         table.getColumns().addAll(drinkNameCol, personNameCol, drinksHadCol, ouncesHadCol);
+
+        table.setRowFactory( tv -> {
+            TableRow<OrderData> row = new TableRow<>();
+            row.setOnMouseClicked(event -> {
+                if (event.getClickCount() == 2 && (! row.isEmpty()) ) {
+                    String name = row.getItem().getPersonName();
+
+                    
+                    //any code here @jordan
+
+
+                    table.getItems().remove(row.getItem());
+                }
+            });
+            return row ;
+        });
 
         final VBox vbox = new VBox();
         vbox.setSpacing(5);
