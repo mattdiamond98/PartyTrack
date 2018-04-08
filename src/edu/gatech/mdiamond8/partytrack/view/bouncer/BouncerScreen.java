035c5027e5b4e508a39293e0f13e08fa10ea6e5d
Matan Diamond <mattdiamond98@gmail.com>
2018-04-08 02:16:09 -0400

    its really late wo

--- src/edu/gatech/mdiamond8/partytrack/view/bouncer/BouncerScreen.java
+++ src/edu/gatech/mdiamond8/partytrack/view/bouncer/BouncerScreen.java
@@ -1,7 +1,5 @@
 package edu.gatech.mdiamond8.partytrack.view.bouncer;
 
-import edu.gatech.mdiamond8.partytrack.qr.OtherReader;
-import edu.gatech.mdiamond8.partytrack.qr.QRImage;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Parent;
