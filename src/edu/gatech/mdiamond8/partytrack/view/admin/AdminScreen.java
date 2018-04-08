a282b6c0fb1da5fd98a86c59311b3377ed86ac4b
Matan Diamond <mattdiamond98@gmail.com>
2018-04-07 21:21:49 -0400

    even more gui stuff

--- src/edu/gatech/mdiamond8/partytrack/view/admin/AdminScreen.java
+++ src/edu/gatech/mdiamond8/partytrack/view/admin/AdminScreen.java
@@ -1,10 +1,8 @@
-package edu.gatech.mdiamond8.partytrack.view.bartender;
+package edu.gatech.mdiamond8.partytrack.view.admin;
 
-import edu.gatech.mdiamond8.partytrack.view.HomeScreen;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
@@ -16,42 +14,40 @@ import javafx.scene.text.FontWeight;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
-public class DrinksScreen {
+public class AdminScreen {
 
     public static Parent getParent(Stage primaryStage) {
+
         GridPane grid = new GridPane();
         grid.setAlignment(Pos.CENTER);
         grid.setHgap(10);
         grid.setVgap(10);
         grid.setPadding(new Insets(25, 25, 25, 25));
 
-        Text scenetitle = new Text("Drinks");
+        Text scenetitle = new Text("New Drinks");
         scenetitle.setFont(Font.font("Tahoma", FontWeight.NORMAL, 20));
         grid.add(scenetitle, 0, 0, 2, 1);
 
-        Label userName = new Label("Drink:");
+        Label userName = new Label("Drink Name:");
         grid.add(userName, 0, 1);
 
         TextField userTextField = new TextField();
         grid.add(userTextField, 1, 1);
 
-        Label pw = new Label("UrMomGay:");
+        Label pw = new Label("Alcohol Content:");
         grid.add(pw, 0, 2);
 
         PasswordField pwBox = new PasswordField();
         grid.add(pwBox, 1, 2);
 
-        Button btn = new Button("Scan Wristband");
+        Button btn = new Button("Add New Drink");
         HBox hbBtn = new HBox(10);
         hbBtn.setAlignment(Pos.BOTTOM_RIGHT);
         hbBtn.getChildren().add(btn);
         grid.add(hbBtn, 1, 4);
 
         btn.setOnAction(e -> {
-            /**
-             * TODO: Sklar add code here
-             */
-            System.exit(0); //i.e. code that isn't this
+            //TODO: add code that will let admin input drink
         });
         return grid;
 
